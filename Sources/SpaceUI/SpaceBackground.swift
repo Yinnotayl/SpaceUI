45c6224aff74808552e9862f05b989f290b50f3c
Yinnotayl <yijue20201@gmail.com>
2026-04-17 16:56:04 +0800

    Enhance SpaceBackground for cross-platform support
    Refactor SpaceBackground to support both UIKit and AppKit. Update spaceBackground modifier for improved clarity.
    Removed the usage of Image(_:) since this package doesn’t use Xcode Assets

--- Sources/SpaceUI/SpaceBackground.swift
+++ Sources/SpaceUI/SpaceBackground.swift
@@ -1,40 +1,39 @@
 import SwiftUI
 
 // SpaceBackground.swift
-// Contains custom background View and View modifier
+// Contains custom background View struct and View modifier
 
 public struct SpaceBackground: View {
     public init() {}
     public var body: some View {
+        #if canImport(UIKit)
         if let uiImage = UIImage(named: "SpaceBackground.png", in: .module, with: nil) {
             Image(uiImage: uiImage)
                 .resizable()
                 .aspectRatio(contentMode: .fill)
-                .frame(minWidth: 0, maxWidth: .infinity)
                 .edgesIgnoringSafeArea(.all)
         }
-        // Image("SpaceBackground", bundle: .module)
-            // .resizable()
-            // .aspectRatio(contentMode: .fill)
-            // .frame(minWidth: 0, maxWidth: .infinity)
-            // .edgesIgnoringSafeArea(.all)
+        #elseif canImport(AppKit)
+        if let nsImage = Bundle.module.image(forResource: "SpaceBackground.png") {
+            Image(nsImage: nsImage)
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+                .edgesIgnoringSafeArea(.all)
+        }
+        #endif
     }
 }
+
 extension View {
     @ViewBuilder
     public func spaceBackground(clipped: Bool = false) -> some View {
-        Group {
-            if clipped {
-                self.background(
-                    Image("SpaceBackground", bundle: .module)
-                        .resizable()
-                        .aspectRatio(contentMode: .fill)
-                )
-            } else {
-                ZStack {
-                    SpaceBackground()
-                    self
-                }
+        if clipped {
+            self.background(SpaceBackground())
+                .clipped()
+        } else {
+            ZStack {
+                SpaceBackground()
+                self
             }
         }
     }
